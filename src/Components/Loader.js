06a3b427e127ff2d8a2ad6308be6f403a31a25e4
Doaa Essam <due_essam_2015@yahoo.com>
2022-04-19 03:13:00 +0200

    Init home page , create loader , fetch data from backend

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -2,12 +2,10 @@ import React from "react";
 
 function Loader() {
   return (
-    <div className="wrapper">
-      <span className="circle circle-1"></span>
-      <span className="circle circle-2"></span>
-      <span className="circle circle-3"></span>
-      <span className="circle circle-4"></span>
-      <span className="circle circle-5"></span>
+    <div className="loader-container">
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
   );
 }
